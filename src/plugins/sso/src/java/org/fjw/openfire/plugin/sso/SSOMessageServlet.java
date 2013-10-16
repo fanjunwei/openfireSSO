ec85a945bf27e5b2e7b0c7a99c8b01444728819a
fanjunwei <fanjunwei9140@gmail.com>
2013-10-16 17:26:43 +0800

    信息发送接口

--- src/plugins/sso/src/java/org/fjw/openfire/plugin/sso/SSOMessageServlet.java
+++ src/plugins/sso/src/java/org/fjw/openfire/plugin/sso/SSOMessageServlet.java
@@ -9,26 +9,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.fjw.openfire.plugin.SSOPlugin;
 import org.jivesoftware.admin.AuthCheckFilter;
+import org.jivesoftware.openfire.SessionManager;
 import org.jivesoftware.openfire.XMPPServer;
-import org.jivesoftware.openfire.roster.Roster;
-import org.jivesoftware.openfire.roster.RosterItem;
-import org.jivesoftware.openfire.user.UserManager;
-import org.jivesoftware.openfire.user.UserNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xmpp.packet.JID;
 
-public class SSORosterServlet extends HttpServlet {
+public class SSOMessageServlet extends HttpServlet {
 
 	private static final Logger Log = LoggerFactory
-			.getLogger(SSORosterServlet.class);
+			.getLogger(SSOMessageServlet.class);
 
-	private SSOPlugin plugin;
 	private XMPPServer server;
-	private UserManager userManager;
+	private SessionManager sessionManager;
 
-	public SSORosterServlet() {
+	public SSOMessageServlet() {
 
 	}
 
@@ -36,33 +32,26 @@ public class SSORosterServlet extends HttpServlet {
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
 		server = XMPPServer.getInstance();
-		userManager = server.getUserManager();
-		plugin = (SSOPlugin) XMPPServer.getInstance().getPluginManager()
-				.getPlugin("sso");
-		AuthCheckFilter.addExclude("sso/roster");
+		sessionManager = server.getSessionManager();
+		AuthCheckFilter.addExclude("sso/message");
 	}
 
 	@Override
 	public void destroy() {
 		super.destroy();
-		AuthCheckFilter.removeExclude("sso/roster");
+		AuthCheckFilter.removeExclude("sso/message");
 	}
 
 	@Override
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		String jid = request.getParameter("jid");
 		PrintWriter out = response.getWriter();
-		try {
-			Roster cachedRoster = userManager.getUser(jid).getRoster();
-			for (RosterItem item : cachedRoster.getRosterItems()) {
-				out.println(item.getJid());
-				out.println(item.getNickname());
-			} 
-		} catch (UserNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		String jid = request.getParameter("jid");
+		String msg = request.getParameter("msg");
+		out.println(jid);
+		out.println(msg);
+		sessionManager.sendServerMessage(new JID(jid), null, msg);
+		out.println("ok");
 	}
 
 	@Override
